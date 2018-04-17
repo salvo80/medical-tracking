ccb5468468a67019efca44758f0e87f148d3d9f9
lupo <salvatore.lupo@eng.it>
2018-04-17 17:09:33 +0200

    localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AccountPage } from '../pages/account/account';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   templateUrl: 'app.html'
@@ -10,10 +11,17 @@ import { AccountPage } from '../pages/account/account';
 export class MyApp {
   rootPage:any = AccountPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private translate: TranslateService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      
+      //translate.addLangs(["it"]);
+      translate.setDefaultLang('it');
+      //let browserLang = translate.getBrowserLang();
+      //translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+      translate.use('it')
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
